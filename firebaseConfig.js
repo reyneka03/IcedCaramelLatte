7ed356576ae13995cf98121349874268ffc3474b
reyneka joe <reynekajoe03@gmail.com>
2024-04-18 23:05:15 +0000

    firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDTISTFMoQ5iHeT5c-8aibjtdGU8rOqPmQ",
+    authDomain: "iced-vanilla-latte.firebaseapp.com",
+    projectId: "iced-vanilla-latte",
+    storageBucket: "iced-vanilla-latte.appspot.com",
+    messagingSenderId: "880980474986",
+    appId: "1:880980474986:web:874b2f27ae292792e7de13"
+  };
+
+  export default firebaseConfig
