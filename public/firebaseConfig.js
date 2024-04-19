aadca8a31379d53ac7c029b052de011a8351373b
reyneka joe <reynekajoe03@gmail.com>
2024-04-19 10:27:54 +0000

    commit 4

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDTISTFMoQ5iHeT5c-8aibjtdGU8rOqPmQ",
-    authDomain: "iced-vanilla-latte.firebaseapp.com",
-    projectId: "iced-vanilla-latte",
-    storageBucket: "iced-vanilla-latte.appspot.com",
-    messagingSenderId: "880980474986",
-    appId: "1:880980474986:web:874b2f27ae292792e7de13"
-  };
+  apiKey: "AIzaSyDTISTFMoQ5iHeT5c-8aibjtdGU8rOqPmQ",
+  authDomain: "iced-vanilla-latte.firebaseapp.com",
+  projectId: "iced-vanilla-latte",
+  storageBucket: "iced-vanilla-latte.appspot.com",
+  messagingSenderId: "880980474986",
+  appId: "1:880980474986:web:7719d88342de722ce7de13"
+};
+
 
   export default firebaseConfig;
